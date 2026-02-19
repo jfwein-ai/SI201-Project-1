50aecf93e5014a71b40d4f1db8be2260b159f68e
Jared Weingarten <jfwein@umich.edu>
2026-02-19 14:41:17 -0500

    Noam Function 1

--- p1-starter.py
+++ p1-starter.py
@@ -12,7 +12,51 @@ import csv
 import os
 
 current_dir = os.path.dirname(__file__)
-file_path = os.path.join(current_dir, "data.csv")
+file_path = os.path.join(current_dir, "penguins.csv")
 
-with open("penguins.csv", "r") as file:
-    reader = csv.DictReader(file)
+with open(file_path, "r") as data:
+    reader = csv.DictReader(data)
+    rows = list(reader)
+
+
+
+for row in rows:
+    species = row['species']
+    print(species)
+
+def avg_body_mass_by_species_island(data):
+    # For each species, what is the average body mass on each island? (Noam)
+	# Columns: “species”, “Island”, and “body_mass_g”
+
+    result = {}
+
+    for row in data:
+        species = row["species"]
+        island = row["island"]
+        mass = row["body_mass_g"]
+
+        if mass == "" or mass == "NA":
+            continue
+
+        mass = float(mass)
+
+        if species not in result:
+            result[species] = {}
+            
+        if island not in result[species]:
+            result[species][island] = {"total": 0, "count": 0}
+
+        result[species][island]["total"] += mass
+        result[species][island]["count"] += 1
+
+    for species in result:
+        for island in result[species]:
+            total = result[species][island]["total"]
+            count = result[species][island]["count"]
+            result[species][island] = total / count
+
+    return result
+
+result = avg_body_mass_by_species_island(rows)
+print(result)
+
